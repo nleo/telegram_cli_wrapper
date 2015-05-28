093d3f96d6fa1ffc344874f9a754a2a4fc4f9952
nleo <nlpub0@gmail.com>
2015-05-28 06:45:56 +0300

    unread_messages

--- lib/telegram_cli_wrapper.rb
+++ lib/telegram_cli_wrapper.rb
@@ -13,8 +13,8 @@ class TelegramCliWrapper
     @socket.puts command
     while str = @socket.gets
       puts "SOCKET READ #{str}" if @vebose
-      if str != '\n' && !str['ANSWER']
-        return JSON.parse str
+      if str != "\n" && !str['ANSWER']
+        return JSON.parse str, object_class: OpenStruct
       end
     end
   end
@@ -28,9 +28,25 @@ class TelegramCliWrapper
   end
 
   def unread_messages
+    unread_messages = []
+    contacts.each do |contact|
+      history(contact['print_name']).each do |message|
+        unread_messages << message if message['unread']
+      end
+    end
+    unread_messages
+  end
+
+  def respond message, text
+    send_message message['from']['print_name'], 51.3
   end
 
   def send_message user, text
+    exec "msg #{user} #{text}"
+  end
+
+  def history user
+    exec "history #{user}"
   end
 
 end
